15efcdbceda52ff50cf305af9ef9600c8dff0d3f
tejasvi <saitejasvi14@gmail.com>
2017-11-08 12:47:42 -0800

    .
    .

--- android-coding-challenge/app/src/main/java/com/stashinvest/stashchallenge/api/GettyImageService.java
+++ android-coding-challenge/app/src/main/java/com/stashinvest/stashchallenge/api/GettyImageService.java
@@ -19,6 +19,8 @@ public class GettyImageService {
     }
 
     public Call<ImageResponse> searchImages(String phrase) {
+
+        
         return api.searchImages(phrase, FIELDS, SORT_ORDER);
     }
 
